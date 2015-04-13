4f4f670ef5ee0e8dfdf9946637434ea4806e1980
Jordi Pamies <jordipamies2@gmail.com>
2015-04-13 19:46:13 +0100

    programando botones

---
+++ static/js/main.js
@@ -0,0 +1,51 @@
+// SlideToggle
+
+
+$( ".MainMenu-icon" ).click(function() {
+  $( ".MainMenu" ).slideToggle( "" );
+});
+
+$( ".NextEvent-toggle" ).click(function() {
+  $( ".NextEvent-detail" ).slideToggle( "" );
+});
+
+
+
+// jQuery on document loaded
+
+$(window).load(function(){
+	
+  if(window.innerWidth<700){
+    $('.MainSection-cta').addClass('btn-small')
+  } else if(window.innerWidth>700){
+  	$('.MainSection-cta').addClass('btn-big')
+  }
+})
+
+
+
+// jQuery on document ready
+
+$(document).ready(function(){
+
+	// Mostrar menu en bo grande
+	$(window).resize(function(){
+	  if(window.innerWidth>=1001){
+	 	$('.MainMenu').css({'display':'block'})
+	}else if(window.innerWidth<1001){
+	  $('.MainMenu').css({'display':'none'})}
+	});
+
+	// $(window).resize(function(){
+	//   if(window.innerWidth>700){
+	//     $('.MainSection-cta').removeClass('btn-small');
+	//     $('.MainSection-cta').addClass('btn-big')
+	//   } else if(window.innerWidth<700){
+	//     $('.MainSection-cta').removeClass('btn-big');
+	//     $('.MainSection-cta').addClass('btn-small')
+	//   }
+	// })
+
+
+})
+

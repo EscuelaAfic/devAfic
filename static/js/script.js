8e554a4016161b4014412fae4b09780bfe1adc1f
Jordi Pamies <jordipamies2@gmail.com>
2015-04-13 02:35:11 +0100

    cambiar tamaño btn

--- static/js/script.js
+++ static/js/script.js
@@ -9,7 +9,15 @@ $( ".NextEvent-toggle" ).click(function() {
   $( ".NextEvent-detail" ).slideToggle( "" );
 });
 
+$('#MainSection-cta').on('mouseover', function(){
+	this.className('MainSection-cta btn btn-big btn-Green')
+})
 
+$('#MainSection-cta').on('mouseout', function(){
+	this.className('MainSection-cta btn btn-big btn-Red')
+})
+
+// jQuery on document ready
 
 $(document).ready(function(){
 
@@ -19,6 +27,8 @@ $(document).ready(function(){
 	}else if(window.innerWidth<1001){
 	  $('.MainMenu').css({'display':'none'})}
 	})
+
+
 
 })
 

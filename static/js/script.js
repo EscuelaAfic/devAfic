021f5f0d7a11303afd2409f61dd4f4cd43e0f2c5
Jordi Pamies <jordipamies2@gmail.com>
2015-04-13 02:04:36 +0100

    *

--- static/js/script.js
+++ static/js/script.js
@@ -16,7 +16,8 @@ $(document).ready(function(){
 $(window).resize(function(){
   if(window.innerWidth>=1001){
  $('.MainMenu').css({'display':'block'})
-	}
+}else if(window.innerWidth<1001){
+  $('.MainMenu').css({'display':'none'})}
 })
 
 })
